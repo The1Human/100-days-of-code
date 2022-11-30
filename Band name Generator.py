eb09aff42134cb0b011b72266ad24d8fbd624d5c
The1Human <99347502+The1Human@users.noreply.github.com>
2022-11-30 07:16:30 +0200

    Variables, Data Types and string manipulation

---
+++ Band name Generator.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Good day! And welcome to the Band Name Generator.")
+#2. Ask the user for the city that they grew up in.
+city = input("Please enter the city in which you grew up in.\n")
+print(city)
+#3. Ask the user for the name of a pet.
+pet = input("Please enter the name of your pet.\n")
+print(pet)
+#4. Combine the name of their city and pet and show them their band name.
+Bandname = print('Your band name is ' + city + " " + pet)
+#5. Make sure the input cursor shows on a new line:
+###use a /n during input layer
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
